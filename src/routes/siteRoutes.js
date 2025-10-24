32b64809a96dbbbbed2c234fb7e61c03a94e8e14
vicecersa <taufiq@spectreinnovations.com>
2025-10-25 00:07:43 +0700

    fix bug

--- src/routes/siteRoutes.js
+++ src/routes/siteRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getMetaByPageId, updateMetaByPageId } = require('../controllers/siteController');
 
-router.get('/meta/', getMetaByPageId);
-router.put('/meta/', updateMetaByPageId);
+router.get('/meta', getMetaByPageId);
+router.put('/meta', updateMetaByPageId);
 
 module.exports = router;
